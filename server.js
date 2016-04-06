c754d38c4e8e27d14a967137df88c774fa3693ed
Laura <lg3295@gmail.com>
2016-04-06 12:31:27 -1000

    adds case for POST method

--- server.js
+++ server.js
@@ -8,20 +8,39 @@ var server = http.createServer(function(req, res){
   var date = new Date();
   date = date.toUTCString();
 
-  fs.readFile(path, function(err, data){
-    if (err){
-      console.log('theres been a problem, sorry');
-      console.log('err',err);
+  if (method === 'GET'){
+    fs.readFile(path, function(err, data){
+      if (err){
+        console.log('theres been a problem, sorry');
+        console.log('err',err);
+      }
+      else {
+        res.write(
+          "HTTP/1.1 200 OK \n" +
+          "date: " + date + "\n" +
+          "server: LG Servers \n\n" + data);
+      }
+    }); //Ends fs.readFile
+  } //Ends If Statement
+
+  if (method === 'POST'){
+    if (path.indexOf('.') !== -1){
+      path = path + ".html";
     }
-    else {
-      res.write(
-        "HTTP/1.1 200 OK \n" +
-        "date: " + date + "\n" +
-        "server: LG Servers \n\n" + data);
-      res.end();
-    }
-  });
+    var newFile = fs.createWriteStream(path);
+    res.pipe(newFile);
+    res.on('end', function(){
+      newFile.end();
+    });
+  }
+  res.write(
+    "HTTP/1.1 200 OK \n" +
+    "content-type: application/json \n" +
+    "date: " + date + "\n" +
+    "server: LG Servers \n\n" + {'Success' : true});
+  res.end();
 }); //Ends Server
+
 
 server.listen({port: 8080}, function(){
   var address = server.address();
