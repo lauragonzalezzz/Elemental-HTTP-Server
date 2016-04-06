2f9af0eb234109f5c00eef66f410b30375ddf207
Laura <lg3295@gmail.com>
2016-04-06 12:05:18 -1000

    responds to correct requests with data

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(res){
-  res.on('data', function(){
-    var path = http.request('path', function(){
+var server = http.createServer(function(req, res){
+  method = req.method;
+  path = "." + req.url;
+
+  var date = new Date();
+  date = date.toUTCString();
+
+  fs.readFile(path, function(err, data){
+    if (err){
+      console.log('theres been a problem, sorry');
+      console.log('err',err);
+    }
+    else {
       res.write(
         "HTTP/1.1 200 OK \n" +
-        "Date: " + date + "\n" +
-        "Server: LG Servers \n\n" + path);
-    });
+        "date: " + date + "\n" +
+        "server: LG Servers \n\n" + data);
+      res.end();
+    }
   });
 }); //Ends Server
 
 server.listen({port: 8080}, function(){
   var address = server.address();
 });
+
+// function returnError(res, date){
+//   res.write(
+//     "HTTP/1.1 404 Not Found \n" +
+//     "date: " + date + "\n" +
+//     "server: LG Servers \n\n");
+// };
