d887f6ac989ade7734681591b6d17ada1afcfdd5
Laura <lg3295@gmail.com>
2016-04-07 19:44:54 -1000

    requires authorization credentials for POST, PUT, DELETE methods

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var AUTH = require('./private.js');
 
 var server = http.createServer(function(req, res){
   var method = req.method;
@@ -23,6 +24,24 @@ var server = http.createServer(function(req, res){
       }
     }); //Ends fs.readFile
   } //Ends If Statement
+
+  if (!req.headers.authorization){ //If no auth header, no access
+    res.writeHead(401, {
+      "WWW-Authenticate": "Basic realm='Secure Area'"
+    });
+    return res.end('<html><body>Not Authorized</body></html>');
+  } //Ends if AUTH
+  else { //If auth header exists, check user/pass
+    var encodedString = req.headers.authorization.replace("Basic ", "");
+    var base64Buffer = new Buffer(encodedString, "base64");
+    var decodedString = base64Buffer.toString();
+    if (decodedString !== AUTH){
+      res.writeHead(401, {
+      "WWW-Authenticate": "Basic realm='Secure Area'"
+    });
+    return res.end('<html><body>Invalid Authentication Credentials</body></html>');
+    }
+  } //Ends else AUTH
 
   if (method === 'POST'){
     if (path !== "/elements"){
